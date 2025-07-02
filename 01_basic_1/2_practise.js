07d6c04df65c8d55996c3cecbca48eb9cb994da7
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-07-02 18:18:23 +0000

    prctise_2

---
+++ 01_basic_1/2_practise.js
@@ -0,0 +1,48 @@
+  let n1;
+  console.log(n1);
+
+  //number data type
+
+  let n2=null;
+    console.log(n2);
+let teju={
+  name:"Tejasave",
+  class:"6th",
+  baaby:null
+  
+
+}
+console.log(teju)
+let n1 = 2;
+console.log(n1)
+
+let n2 = 1.3;
+console.log(n2)
+
+let n3 = Infinity;
+console.log(n3)
+
+let n4 = 'something here too' / 2;
+console.log(n4)
+let s1 = "Hello There";
+console.log(s1); 
+
+let s2 = 'Single quotes work fine';
+console.log(s2); 
+
+let s3 = `can embed ${s1}`;
+console.log(s3);
+
+let a;
+console.log(a);
+let s1 = Symbol("Geeks");
+let s2 = Symbol("Geeks");
+console.log(s1 == s2);
+let b = BigInt("0b1010101001010101001111111111111111");
+console.log(b);
+
+let gfg = {
+    type: "Company",
+    location: "Noida"
+}
+console.log(gfg.type)
