7637fac259c251469e1c3f4810b1e8aa1ad46dc8
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-07-03 17:47:40 +0000

    new

--- 01_basic_1/4_practise.js
+++ 01_basic_1/4_practise.js
@@ -2,3 +2,10 @@ console.log("1"+1+"1")
 console.log("1"+(1+1+1))
 
 console.log(1*2*3+1+"1")
+console.log("22"<1)
+
+console.log(null>=0)
+console.log(null===0)
+console.log(null<=0)
+
+//due to type conversion it come true
