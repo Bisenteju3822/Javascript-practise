4555e1cc5d55f6f802bc5638506759305d5f6f97
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-07-06 15:33:31 +0000

    new:

---
+++ 01_basic_1/7_practise.js
@@ -0,0 +1,29 @@
+let name ="teju";
+const age=22;
+console.log(name+22);
+console.log(`hello boy ${name}`);
+ name =new String('pallav-hc')
+
+//console.log(gameName[0]);
+//console.log(gameName._proto_.)
+console.log(name.toUpperCase())
+console.log(name.charAt(1))
+console.log(name.indexOf('l'))
+const newString=name.substring(0,4)
+console.log(newString)
+const anotherString=name.slice(-6,4)
+console.log(anotherString)
+
+const newStringONe="  hitesh   " 
+console.log(newStringONe.trim());
+
+//trim basically remove unwanted space
+
+
+
+
+
+
+
+
+
