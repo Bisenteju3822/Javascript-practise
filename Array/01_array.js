37182fac18b9e15260456662d24f489bb1df0ba4
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-07-08 17:55:39 +0000

    new

---
+++ Array/01_array.js
@@ -0,0 +1,23 @@
+const myArr=[0,1,2,4,5]
+// const myHeors=["shaktiman","naagraj"]
+
+
+// const myArr2=new Array(1,2,3,4)
+// console.log(myArr[1])
+
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop()
+//console.log(myArr)
+// myArr.unshift(9)
+// //myArr.shift()
+// console.log(myArr)
+
+// const newArr=myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+
+//slice ,splice
+
+console.log("A",myArr)
